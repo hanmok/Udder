d71b311063241cf794e7194855604278b74c56fd
hanmok <57804416+hanmok@users.noreply.github.com>
2020-05-10 12:46:15 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by hanmok on 2020/05/10.
+//  Copyright © 2020 hanmok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
